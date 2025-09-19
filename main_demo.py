0cfe043753fa541b6d07d3f79a4ed51fb333d8eb
Jon Pang <jp2268@cornell.edu>
2025-09-18 23:43:31 -0700

    generate and plot data that is better suited for training

--- main_demo.py
+++ main_demo.py
@@ -1,6 +1,8 @@
 import utils.perceptron_utils as utils
 
 def main():
-  utils.plot_random_data()
+  # utils.plot_random_data()
+  X, Y = utils.generate_data()
+  utils.plot_X(X)
 
 main()
--- utils/perceptron_utils.py
+++ utils/perceptron_utils.py
@@ -11,5 +11,34 @@ def plot_random_data():
   ax.scatter(3*generate_random_data(10), 2*generate_random_data(10), c='red')
   plt.show()
 
-def slp_infer(x, w, b):
-  return np.round(x*w+b)
+# Assumes y in {-1, 1}
+def training_loop(X, Y, rate, epochs):
+  w = default_rng().random((1,2))
+  b = default_rng().random()
+  for _ in range(epochs):
+    for i in range(len(X)):
+      x = X[i]
+      y = Y[i]
+      w = w + rate*y*x
+      b = b + rate*y
+      print(w, b)
+  return w, b
+
+def generate_data():
+  positive = default_rng().random((10, 2))
+  skew = np.array([[3,0],[0,2]])
+  positive = positive.dot(skew)
+  negative = default_rng().random((10, 2))
+  X = np.concatenate((positive, negative), axis=0)
+  Y = np.concatenate((np.ones(10), -np.ones(10)))
+  return X, Y
+
+def plot_X(X):
+  fig, ax = plt.subplots()
+  positive = X[:10]
+  negative = X[10:]
+  for x in positive:
+    ax.scatter(x[0], x[1], c='blue')
+  for x in negative:
+    ax.scatter(x[0], x[1], c='red')
+  plt.show()
